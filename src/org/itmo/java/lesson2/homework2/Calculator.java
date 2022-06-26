ce260c2d1512fe933e82c867ecb55c5badd4dc34
Andrey.Zholobov <andreizholobov901@gmail.com>
2022-06-26 17:30:19 +0300

    Lesson 2

---
+++ src/org/itmo/java/lesson2/homework2/Calculator.java
@@ -0,0 +1,55 @@
+package org.itmo.java.lesson2.homework2;
+
+public class Calculator {
+    //Суммирование
+    public static int summ(int a, int b) {
+        return a + b;
+    }
+
+    public static double summ(double a, double b) {
+        return a + b;
+    }
+
+    public static long summ(long a, long b) {
+        return a + b;
+    }
+
+    //Вычитание
+    public static int extr(int a, int b) {
+        return a - b;
+    }
+
+    public static double extr(double a, double b) {
+        return a - b;
+    }
+
+    public static long extr(long a, long b) {
+        return a - b;
+    }
+
+    //Умножение
+    public static int Multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double Multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static long Multiply(long a, long b) {
+        return a * b;
+    }
+
+    //Деление
+    public static double div(int a, int b) {
+        return (double) a / b;
+    }
+
+    public static double div(double a, double b) {
+        return a / b;
+    }
+
+    public static double div(long a, long b) {
+        return (double) a / b;
+    }
+}
