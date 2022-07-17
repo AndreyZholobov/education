37409e480a0691da5bc53ebdf5c8625907e61ffb
Andrey.Zholobov <andreizholobov901@gmail.com>
2022-07-17 12:43:11 +0300

    homework 6 7 8

--- src/org/itmo/java/lesson4/homework4/DivisionNumber35.java
+++ src/org/itmo/java/lesson4/homework4/DivisionNumber35.java
@@ -2,8 +2,8 @@ package org.itmo.java.lesson4.homework4;
 
 public class DivisionNumber35 {
     public static void divisionNumber35() {
-        //2.	Напишите программу, которая выводит числа от 1 до 100, которые делятся на 3, 5 и на то и другое
-        // (то есть и на 3 и на 5).
+        //2.	Напишите программу, выводит числа от 1 до 100, которые делятся на 3, 5 и на то и другое
+        // (то-есть и на 3 и на 5).
         System.out.println("Делится на 3");
         for (int i = 1; i < 100; i++) {
             if (i % 3 == 0) {
